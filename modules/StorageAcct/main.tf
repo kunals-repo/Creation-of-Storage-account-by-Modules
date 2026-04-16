4dd0201f0d210e0c952864bf0ebf4c731c5ea074
Kunal Yeole <kunalpatil9324@gmail.com>
2026-04-16 19:23:20 +0530

    Storage Acct module's main.tf file added

---
+++ modules/StorageAcct/main.tf
@@ -0,0 +1,12 @@
+
+resource "azurerm_storage_account" "storage-block" {
+  name                     = var.storage-name
+  resource_group_name      = var.rg-name
+  location                 = var.rg-location
+  account_tier             = "Premium"
+  account_replication_type = var.replication-type
+  account_kind             = "BlockBlobStorage"
+
+  tags = var.tags
+
+}
